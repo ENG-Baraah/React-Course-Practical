68929457b573cc751aa363c6bea915098309db3f
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2024-02-05 21:43:21 +0300

    Add Header and CoreConcept components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
+import Header from "./Header";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+
+function CoreConcept({ title, image, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+//Alternative way
+//function CoreConcept(props) {
+// return (
+//   <li>
+//     <img src={props.image} alt={props.title} />
+//     <h3>{props.title}</h3>
+//     <p>{props.description}</p>
+//   </li>
+// );
+// }
+//----
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core-Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/* Alternative way */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
