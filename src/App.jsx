322615f5b00c4cf9384cb299bf4b85a07c3c1de3
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2024-02-22 00:45:10 +0300

    Component Composition: The special children  Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "../src/components/Header/Header.jsx";
 import CoreConcept from "../src/components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
   return (
@@ -21,7 +22,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Prop</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
